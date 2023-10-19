43e8b80fef950652652f07219aa780a7b9460224
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-10-19 16:36:01 -0300

    feliz

--- app/cadastro/page.jsx
+++ app/cadastro/page.jsx
@@ -1,13 +1,14 @@
 "use client"
 import { useState } from "react"
 import Cadastro from "../components/Cadastro/Cadastro";
+import styles from "../cadastro/cadastro.module.css"
 
 const cadastro = () => {
     const [frase, setFrase] = useState('');
     const [author, setAuthor] = useState('');
 
     return (
-        <div>
+        <div className={styles.all}>
             <Cadastro buttonText={'Registrar'} textFrase={'Registrar frase'} textAuthor={'Autor da frase'} frase={frase} author={author} setFrase={setFrase} setAuthor={setAuthor}/>
         </div>
     )
--- app/components/Cadastro/Cadastro.jsx
+++ app/components/Cadastro/Cadastro.jsx
@@ -1,12 +1,16 @@
 import Input from "../Input/Input"
 import styles from './cadastro.module.css'
 
-const Cadastro = ({buttonText, onClick, textFrase, textAuthor, author, frase, setFrase, setAuthor, className}) => {
+const Cadastro = ({buttonText, onClick, textFrase, textAuthor, author, frase, setFrase, setAuthor}) => {
     return (
-        <div>
+        <div className={styles.all}>
+            <section>
             <Input className={styles} text={textFrase} value={frase} setValue={setFrase} />
             <Input className={styles} text={textAuthor} value={author} setValue={setAuthor} />
+            </section>
+            <section>
             <button onClick={onClick}>{buttonText}</button>
+            </section>
         </div>
     )
 }
