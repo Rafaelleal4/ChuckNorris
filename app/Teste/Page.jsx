56a65d59250ec95625013b5850ea4b3f8c1a47d2
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-19 16:59:11 -0300

    feat: começo do map da lista

--- app/Teste/Page.jsx
+++ app/Teste/Page.jsx
@@ -1,13 +1,40 @@
+"use client"
 import { useState } from "react";
+import { Registro, ListaRegistro } from "@/models/Register";
 import Cadastro from "../components/Cadastro/Cadastro";
 
 const teste = () => {
     const [frase, setFrase] = useState('');
     const [author, setAuthor] = useState('');
 
+    const listaRegistro = new ListaRegistro();
+    
+    const add = () => {
+        const registro = new Registro(frase, author);
+        listaRegistro.add(registro);
+        clean()
+        console.log(listaRegistro.madeComments);
+    }
+
+    const clean = () => {
+        setFrase('')
+        setAuthor('')
+    }
+
     return (
         <div>
-            <Cadastro textFrase={'Registrar frase'} textAuthor={'Autor da frase'} frase={frase} author={author} setFrase={setFrase} setAuthor={setAuthor}/>
+            <Cadastro textFrase={'Registrar frase'} textAuthor={'Autor da frase'} frase={frase} author={author} setFrase={setFrase} setAuthor={setAuthor} buttonText={'Registrar'} onClick={add}/>
+
+            <div>
+                {
+                    listaRegistro.madeComments.map((frase) => (
+                        <div key={frase.id}>
+                            <p>{frase.frase}</p>
+                            <p>{frase.author}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
