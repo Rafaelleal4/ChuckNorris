9fa903eceed1ba2bf223e9f45a66cad2ca695cb1
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-19 17:10:33 -0300

    feat: chamada da frase da api

--- app/Teste/Page.jsx
+++ app/Teste/Page.jsx
@@ -1,11 +1,13 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Registro, ListaRegistro } from "@/models/Register";
+import personagens from "@/data/chuckNorris";
 import Cadastro from "../components/Cadastro/Cadastro";
 
 const teste = () => {
     const [frase, setFrase] = useState('');
     const [author, setAuthor] = useState('');
+    const [dadosApi, setDadosApi] = useState('');
 
     const listaRegistro = new ListaRegistro();
     
@@ -21,18 +23,37 @@ const teste = () => {
         setAuthor('')
     }
 
+    useEffect(() => {
+        const persoFet = async () => {
+            try {
+                const dados = await personagens()
+                setDadosApi(dados)
+                console.log(dados)
+
+            } catch (e) {
+                throw e
+            }
+        };
+        persoFet();
+    }, [])
+
     return (
         <div>
             <Cadastro textFrase={'Registrar frase'} textAuthor={'Autor da frase'} frase={frase} author={author} setFrase={setFrase} setAuthor={setAuthor} buttonText={'Registrar'} onClick={add}/>
 
             <div>
                 {
-                    listaRegistro.madeComments.map((frase) => (
-                        <div key={frase.id}>
-                            <p>{frase.frase}</p>
-                            <p>{frase.author}</p>
+                    dadosApi ? (
+                        <div>
+                            {
+                                <div key={dadosApi.id}>
+                                    <p>{dadosApi.value}</p>
+                                </div>
+                            }
                         </div>
-                    ))
+                    ) : (
+                        <p>carregando API...</p>
+                    )
                 }
             </div>
         </div>
