7e0b10cbf4fa0629f7ca20e071a2995ed1c0b9a2
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-10-24 15:17:17 -0300

    estilizando o footer

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import styles from './footer.module.css'
-import Image from 'next/image'
+import {FaSquareInstagram, FaEnvelope, FaSquareWhatsapp  } from "react-icons/fa6";
 function Footer() {
   return (
     <div className={styles.content}>
         <div className={styles.gif}>
-       <Image 
-       className={styles.kick}
-       src="/chuck-norris-kick-action (1).gif}"
-       width={50}
-       height={50}
-       alt="gif do Chuck Norris chutando"
-       />
+          <img
+            className={styles.kick}
+            src="/chuck-norris-kick-action (1).gif"
+            width={65}
+            height={65}
+            alt="gif do Chuck Norris chutando" />
         </div>
         <div className={styles.conteudo}>
-            <p>todos os direitos reservado</p>
+          <h3>Contatos:</h3>
+          <div className={styles.flex}> 
+            <div>
+                <FaSquareInstagram className={styles.icon}/>
+                <FaEnvelope className={styles.icon}/>
+            </div>
+              <FaSquareWhatsapp className={styles.icon2}/>
+          </div>
+          <div id={styles.texto}>
+            <p id={styles.txt}>todos os direitos reservado</p>
+          </div>
+
         </div>
     </div>
   )
