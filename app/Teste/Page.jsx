350eb7b770480dfb6aaf21fa5f62742d53c07e04
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-24 10:52:19 -0300

    feat: finalização dos objetos pelos inputs

--- app/Teste/Page.jsx
+++ app/Teste/Page.jsx
@@ -3,56 +3,31 @@ import { useState, useEffect } from "react";
 import { Registro, ListaRegistro } from "@/models/Register";
 import personagens from "@/data/chuckNorris";
 import Cadastro from "../components/Cadastro/Cadastro";
+const listaRegistro = new ListaRegistro();
 
 const teste = () => {
     const [frase, setFrase] = useState('');
     const [author, setAuthor] = useState('');
-    const [dadosApi, setDadosApi] = useState('');
-    const listaRegistro = new ListaRegistro();
     
     const add = () => {
         const registro = new Registro(frase, author);
         listaRegistro.add(registro);
-        clean()
-        console.log(listaRegistro.madeComments);
-    }
-
-    const clean = () => {
         setFrase('')
         setAuthor('')
+        console.log(listaRegistro.madeComments);
     }
-
-    useEffect(() => {
-        const persoFet = async () => {
-            try {
-                const dados = await personagens()
-                setDadosApi(dados)
-                console.log(dados)
-
-            } catch (e) {
-                throw e
-            }
-        };
-        persoFet();
-    }, [])
 
     return (
         <div>
             <Cadastro textFrase={'Registrar frase'} textAuthor={'Autor da frase'} frase={frase} author={author} setFrase={setFrase} setAuthor={setAuthor} buttonText={'Registrar'} onClick={add}/>
-
             <div>
                 {
-                    dadosApi ? (
-                        <div>
-                            {
-                                <div key={dadosApi.id}>
-                                    <p>{dadosApi.value}</p>
-                                </div>
-                            }
+                    listaRegistro.madeComments.map((comment) => (
+                        <div key={comment.id}>
+                            <p>{comment.frase}</p>
+                            <p><strong>{comment.author}</strong></p>
                         </div>
-                    ) : (
-                        <p>carregando API...</p>
-                    )
+                    ))
                 }
             </div>
         </div>
