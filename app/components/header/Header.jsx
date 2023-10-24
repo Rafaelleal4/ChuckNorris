d453e84427f52ad36629e76ffbd56325dfb6f5aa
Lucas Rios <lucas.rios@aluno.senai.br>
2023-10-24 14:14:14 -0300

    finalização do header

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -6,22 +6,26 @@ const Header = () => {
         <div className={styles.header}>
 
             <div className={styles.img}>
-            <img src='chuck-norris-write.png' height={80} width={160}></img>
+            <img src='chuck-norris-write.png' height={60} width={240}></img>
             </div>
 
             <div className={styles.categorias}>
             
             <nav>
             <ul className={styles.links}>
+
             <li>
             <Link href='./'>Link1</Link>
             </li>
+            
             <li>
             <Link href='./'>Link2</Link>
             </li>
+
             <li>
             <Link href='./'>Link3</Link>
             </li>
+
             </ul>
             </nav>
             </div>
