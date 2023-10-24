02fe698037dbfd74864a95237005b9e79b908503
Caio Ciccarelli <caio.ciccarelli@aluno.senai.br>
2023-10-24 14:37:43 -0300

    randomizer

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
+"use client"
 import Link from "next/link";
+import Randomizer from "./components/randomizer/Randomizer";
 
 
 export default function Home() {
   return (
  <div>
   <Link href={"tsw_personagens"}>Resfcio</Link>
+  <Randomizer/>
  </div>
   )
 }
