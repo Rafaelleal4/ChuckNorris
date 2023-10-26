73e268dde3dd3234dd1ae1564f686483a4b1c194
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-26 16:49:01 -0300

    feat: card mocado da api

--- app/components/CardsFrases/CardsFrases.jsx
+++ app/components/CardsFrases/CardsFrases.jsx
@@ -1,16 +1,19 @@
-import styles from './cardsFrases.module.css'
-import { Registro ,ListaRegistro } from "@/models/Register";
-import { useState } from 'react';
+import styles from './cardsFrases.module.css';
+import { Registro, ListaRegistro } from "@/models/Register";
+import personagens from '@/data/chuckNorris';
+import Randomizer from '../randomizer/Randomizer';
+import { useState, useEffect } from 'react';
 import { FaHeart } from 'react-icons/fa6'
 
 
-const CardFrases = ({list, exclude}) => {
-    const registro = new Registro(); 
+const CardFrases = ({ list, exclude }) => {
+    const registro = new Registro();
     const [heart, setHeart] = useState(registro.heart)
     const [color, setColor] = useState('#000')
+    const [dadosApi, setDadosApi] = useState('')
 
     const addHeart = (id) => {
-        if(heart == 0) {
+        if (heart == 0) {
             setHeart(1)
             setColor('#d21f3c')
         } else {
@@ -19,15 +22,55 @@ const CardFrases = ({list, exclude}) => {
         }
     }
 
+    useEffect(() => {
+        const persoFet = async () => {
+            try {
+                const dados = await personagens()
+                setDadosApi(dados)
+                console.log(dados)
+
+            } catch (e) {
+                throw e
+            }
+        };
+        persoFet();
+    }, [])
+
     return (
         <div className={styles.list}>
+            <div className={styles.api}>
+                <div>
+                    {
+                        dadosApi ? (
+                            <div>
+                                {
+                                    <div className={styles.cardAPI}>
+                                        <div key={dadosApi.id} className={styles.text}>
+                                            <p><strong>" {dadosApi.value} "</strong></p>
+                                            <p><i>- Chuck Norris</i></p>
+                                        </div>
+                                        <div>
+                                            <button onClick={addHeart} className={styles.heart}><FaHeart style={{ color: color, transition: 'ease-in' }} /> {heart}</button>
+                                            <div>
+                                                <button onClick={exclude(() => comment.id)}>Lixeira</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                        ) : (
+                            <img src="/tumbleweed_idle.webp" alt="tuble weeds" className={styles.load} />
+                        )
+                    }
+                </div>
+            </div>
             {
                 list.map((comment) => (
                     <div key={comment.id} className={styles.card}>
                         <p><strong>" {comment.frase} "</strong></p>
                         <p><i>{`- ${comment.author}`}</i></p>
                         <div>
-                            <button onClick={addHeart} className={styles.heart}><FaHeart style={{color: color, transition:'ease-in'}}/> {heart}</button>
+                            <button onClick={addHeart} className={styles.heart}><FaHeart style={{ color: color, transition: 'ease-in' }} /> {heart}</button>
                             <div>
                                 <button onClick={exclude(() => comment.id)}>Lixeira</button>
                             </div>
