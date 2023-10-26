590ebba7a16a5d0400b9648b725a8421d6d9426c
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-26 09:13:19 -0300

    feat: fiz caquinha e concertei

---
+++ app/components/Cadastro/Cadastro.jsx
@@ -0,0 +1,16 @@
+import styles from './cadastro.module.css'
+
+const Cadastro = ({ buttonText, onClick, textFrase, textAuthor, author, frase, setFrase, setAuthor, list }) => {
+    return (
+        <div className={styles.all}>
+            <h2 className={styles.h2}>Escreva sua frase digna de Chuck Norris</h2>
+            <section className={styles.register}>
+                <input className={styles.inp} placeholder={textAuthor} value={author} onChange={e => setAuthor(e.target.value)} />
+                <input className={styles.inp} placeholder={textFrase} value={frase} onChange={e => setFrase(e.target.value)} />
+                <button className={styles.button} onClick={onClick}>{buttonText}</button>
+            </section>
+        </div>
+    )
+}
+
+export default Cadastro
