d28a90f91e97929d6eec3ee459d6d1cced17a0ab
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-26 08:07:10 -0300

    feat: alterações no design do cadastro

--- app/components/Cadastro/Cadastro.jsx
+++ app/components/Cadastro/Cadastro.jsx
@@ -2,16 +2,15 @@ import Link from "next/link"
 import Input from "../Input/Input"
 import styles from './cadastro.module.css'
 
-const Cadastro = ({buttonText, buttonText2, onClick, textFrase, textAuthor, author, frase, setFrase, setAuthor}) => {
+const Cadastro = ({ buttonText, onClick, textFrase, textAuthor, author, frase, setFrase, setAuthor }) => {
     return (
         <div className={styles.all}>
-            <h2 className={styles.h2}>Escreva sua frase mita</h2>
-            <section>
-            <Input className={styles.inp} text={textAuthor} value={author} setValue={setAuthor} />
-            <button className={styles.button1} onClick={onClick}>{buttonText}</button>
-           <Input className={styles.inp} text={textFrase} value={frase} setValue={setFrase} />
+            <h2 className={styles.h2}>Escreva sua frase digna de Chuck Norris</h2>
+            <section className={styles.register}>
+                <Input className={styles.inp} text={textAuthor} value={author} setValue={setAuthor} />
+                <Input className={styles.inp} text={textFrase} value={frase} setValue={setFrase} />
+                <button className={styles.button} onClick={onClick}>{buttonText}</button>
             </section>
-            <Link href={"tsw_personagens"}> <button className={styles.button2}>{buttonText2}</button></Link>
         </div>
     )
 }
