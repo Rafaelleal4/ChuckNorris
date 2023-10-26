0e9ffa5fbbab567eb0d0e0d96b659380457a1cbd
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-26 08:35:45 -0300

    feat: alterações cadastro e page cadastro

--- app/cadastro/page.jsx
+++ app/cadastro/page.jsx
@@ -1,15 +1,41 @@
 "use client"
 import { useState } from "react"
+import { Registro, ListaRegistro } from "@/models/Register";
 import Cadastro from "../components/Cadastro/Cadastro";
 import styles from "../cadastro/cadastro.module.css"
+import CardFrases from "../components/CardsFrases/CardsFrases";
+const listaRegistro = new ListaRegistro();
 
 const cadastro = () => {
+
     const [frase, setFrase] = useState('');
     const [author, setAuthor] = useState('');
 
+    const add = () => {
+        const registro = new Registro(frase, author);
+        verifyInputs(registro)
+        console.log(listaRegistro.madeComments);
+    }
+
+    const verifyInputs = (registro) => {
+        listaRegistro.emptyInputs(frase, author) ? (
+            alert("Erro"),
+            clean()
+        ) : (
+            alert("Deu bom!!!"),
+            listaRegistro.add(registro),
+            clean()
+        )
+    }
+
+    const clean = () => {
+        setFrase('')
+        setAuthor('')
+    }
+
     return (
         <div className={styles.all}>
-            <Cadastro buttonText={'Registrar'} textFrase={'Registrar frase'} textAuthor={'Autor da frase'} frase={frase} author={author} setFrase={setFrase} setAuthor={setAuthor}/>
+            <Cadastro author={author} frase={frase} setAuthor={setAuthor} setFrase={setFrase} textFrase={'Digite uma frase'} textAuthor={'Autor da frase'} onClick={add} buttonText={'Enviar'} list={listaRegistro.madeComments}/>
         </div>
     )
 }
--- app/components/Cadastro/Cadastro.jsx
+++ app/components/Cadastro/Cadastro.jsx
@@ -1,16 +1,16 @@
-import Link from "next/link"
-import Input from "../Input/Input"
 import styles from './cadastro.module.css'
+import CardFrases from '../CardsFrases/CardsFrases'
 
-const Cadastro = ({ buttonText, onClick, textFrase, textAuthor, author, frase, setFrase, setAuthor }) => {
+const Cadastro = ({ buttonText, onClick, textFrase, textAuthor, author, frase, setFrase, setAuthor, list }) => {
     return (
         <div className={styles.all}>
             <h2 className={styles.h2}>Escreva sua frase digna de Chuck Norris</h2>
             <section className={styles.register}>
-                <Input className={styles.inp} text={textAuthor} value={author} setValue={setAuthor} />
-                <Input className={styles.inp} text={textFrase} value={frase} setValue={setFrase} />
+                <input className={styles.inp} placeholder={textAuthor} value={author} onChange={e => setAuthor(e.target.value)} />
+                <input className={styles.inp} placeholder={textFrase} value={frase} onChange={e => setFrase(e.target.value)} />
                 <button className={styles.button} onClick={onClick}>{buttonText}</button>
             </section>
+            <CardFrases list={list} />
         </div>
     )
 }
