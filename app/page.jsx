41ccb4edcd1c52ae24a6868f722dd2bcbb1a943b
Caio Ciccarelli <caio.ciccarelli@aluno.senai.br>
2023-10-26 08:29:23 -0300

    alteracoes do randomizer e footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,17 @@
 "use client"
 import Link from "next/link";
-
 import Header from "./components/header/Header";
 import Cadastro from "./components/Cadastro/Cadastro";
+import Footer from "./components/footer/Footer";
 
 
 
 export default function Home() {
   return (
- <div>
-
-  <Header />
-  <Cadastro textFrase={"Digite uma frase"} textAuthor={"Nome do Autor"} buttonText={"Enviar"} buttonText2={"Frases Cadastradas"}/>
-
- </div>
+    <div>
+      <Header />
+      <Cadastro textFrase={"Digite uma frase"} textAuthor={"Nome do Autor"} buttonText={"Enviar"} buttonText2={"Frases Cadastradas"} />
+      <Footer />
+    </div>
   )
 }
